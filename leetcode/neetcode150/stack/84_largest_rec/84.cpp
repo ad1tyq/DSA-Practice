b1042f51d8d5665eecfd74c37d8f3869b512c260
ad1tyq <adityabhardwaj9b7@gmail.com>
2026-08-20 19:20:10 +0530

    84 - leetcode

---
+++ leetcode/neetcode150/stack/84_largest_rec/84.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+#include <vector>
+
+class Solution {
+public:
+  int largestRectangleArea(vector<int> &heights) {
+    int area = 0, n = heights.size();
+    stack<int> st;
+    for (int i = 0; i <= n; i++) {
+      int current_height = (i == n) ? 0 : heights[i];
+      while (!st.empty() && current_height < heights[st.top()]) {
+        int height = heights[st.top()];
+        st.pop();
+
+        int width;
+        if (st.empty())
+          width = i;
+        else
+          width = i - st.top() - 1;
+        area = max(area, height * width);
+      }
+      st.push(i);
+    }
+    return area;
+  }
+};
+
+int main() {
+  int n;
+  cout << "enter size: ";
+  cin >> n;
+  cout << "enter heights: ";
+  vector<int> heights(n);
+  for (int i = 0; i < n; i++)
+    cin >> heights[i];
+  Solution *sol = new Solution();
+  int area = (*sol).largestRectangleArea(heights);
+  cout << "area: " << area;
+}
